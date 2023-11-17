5dfd0652138b534758367e1e4b64756bcd67bad8
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-11-17 17:37:37 -0300

    Upload setup files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PatientContainer from './containers/PatientContainer';
+import PatientProvider from './context/PatientContext';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme/theme';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <PatientProvider>
+          <PatientContainer />
+        </PatientProvider>
+      </ThemeProvider>
     </div>
   );
 }
